84a0d367d782fd7216c110babcee4ab213ec49ef
N64Mario <mariogo@gmail.com>
2023-09-27 21:47:32 +0900

    add the server Link

--- src/server.ts
+++ src/server.ts
@@ -29,31 +29,7 @@ socketIo.on("connection", function (socket: any) {
 
   socket.on("send", () => {
     const datas = [
-      "https://wa.me/919863771515",
-      "https://wa.me/916009358837",
-      "https://wa.me/919366570296",
-      "https://wa.me/918787341056",
-      "https://wa.me/+919366395692",
-      "https://wa.me/+918837058351",
-      "https://wa.me/+918787341056",
-      "https://wa.me/+919863970982",
-      "https://wa.me/916009417168",
-      "https://wa.me/919863436782",
-      "https://wa.me/+918837035599",
-      "https://wa.me/918837373371",
-      "https://wa.me/916009222381",
-      "https://wa.me/918787741070",
-      "https://wa.me/919366936594",
-      "https://wa.me/+916009885639",
-      "https://wa.me/+916009569499",
-      "https://wa.me/+917005759591",
-      "https://wa.me/+916009928026",
-      "https://wa.me/+917005955913",
-      "https://wa.me/+919366341553",
-      "https://wa.me/+917005810975",
-      "https://wa.me/+916009626040",
-      "https://wa.me/+919366456244",
-      "https://wa.me/+918119060528",
+      "https://chat.whatsapp.com/Co5vAd3aZTc6HxEOXkGuVx"
     ];
     counter = (counter + 1) % datas.length;
     socket.emit("send__number", datas[counter]);
