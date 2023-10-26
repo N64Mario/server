537e1ab615533eb6e20f0cc63efe6450526f9117
unknown <goafucheng@gmail.com>
2023-10-26 18:52:04 +0400

    return single value

--- src/server.ts
+++ src/server.ts
@@ -2,45 +2,52 @@ import { log } from "console";
 
 const EventEmitter = require("events");
 const myEmitter = new EventEmitter();
+const cors = require("cors");
+const bodyParser = require("body-parser"); // Import the body-parser module
 
 // Increase the maximum number of listeners
 myEmitter.setMaxListeners(200);
-
 var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
-var socketIo = require("socket.io")(http, {
+app.use(cors());
+app.use(bodyParser.json());
+
+const socketIo = require("socket.io")(http, {
   cors: {
     origin: "*",
+    methods: ["GET", "POST"], // Add the HTTP methods you intend to use
   },
 });
 
-http.listen(3000, function () {
-  console.log("Server Started ...");
+const router = express.Router();
+
+// Define a POST route
+let data: any;
+router.post("/send", (req: any, res: any) => {
+  // Handle the POST request here
+  data = req.body.links.map((item: any) => item.link); // You can access the request data from req.body
+  // Perform some actions based on the request data
+  // For example, you can emit a socket event here
+
+  // Send a response back to the client
+  res.json({ message: "POST request received successfully" });
 });
-let counter = 0;
+app.use("/api", router);
+
+let currentIndex = 0;
 // Define your Socket.IO event listeners here
 socketIo.on("connection", function (socket: any) {
-  // Example: Sending a message to the client when they connect
-  socket.emit("welcome", "Welcome to the server!");
-
-  // Define your custom event listeners for this socket
-
-  socket.on("send", () => {
-    const datas = [
-      "https://chat.whatsapp.com/IJanTFlCORc5WC5BZ0FegE",
-      "https://chat.whatsapp.com/Co5vAd3aZTc6HxEOXkGuVx",
-      "https://chat.whatsapp.com/KgvnZKjSyBcArl6oZIMfd2",
-      "https://chat.whatsapp.com/IBSzEMXh3QDATBeiWivNQZ",
-    ];
-    counter = (counter + 1) % datas.length;
-    socket.emit("send__number", datas[counter]);
-
-    // You can emit data back to the client if needed
-    // socket.emit("acknowledge", "Data received");
-  });
-
+  if (data) {
+    console.log(data[currentIndex]);
+    
+    socket.emit("send__links", data[currentIndex]);
+    currentIndex = (currentIndex + 1) % data.length;
+  }
   socket.on("disconnect", () => {
     console.log("A user disconnected");
   });
+});
+http.listen(3000, function () {
+  console.log("Server Started ...");
 });
